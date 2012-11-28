a1d9a7aa241ce7a99289ccb1ec5951d98ae46bd7
kuler <kuler@cersoft.pl>
2012-11-28 23:21:34 +0100

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Kancelaria::Application.config.secret_token = 'f68313acc9425e721c6a1d80568bc410811a75fb0012f7b610dc04432dbd684c672ffdf4f61ce90a46fbf362450d09a1d5e6b9972a8881c84f116ddc9fa25adf'
