66be7133ad08a525fea4c24d2eb463840431054f
Tomasz Frydrychewicz <tfrydrychewicz@gmail.com>
2012-12-08 19:46:51 +0100

    Client model validation

--- app/models/client.rb
+++ app/models/client.rb
@@ -1,4 +1,8 @@
 class Client < ActiveRecord::Base
 	belongs_to :user
 	has_many :cases
+	
+	validates :fullname, presence: true
+	validates :nip, presence: true
+	validates :contact, presence: true
 end
