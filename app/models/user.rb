4b9dbdeb875a75a4e4fc9b491b1b909a3d95cad7
Tomasz Frydrychewicz <tfrydrychewicz@gmail.com>
2012-12-09 10:52:13 +0100

    User authentication User form improved

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,26 @@
 class User < ActiveRecord::Base
-	attr_accessible :username, :password, :password_confirmation, :firstname, :lastname, :active, :admin
-	has_secure_password
-	has_many :clients
+  has_many :clients
+  
+	attr_accessible :password_confirmation, :username, :password, :firstname, :lastname, :active, :admin
+	attr_accessor :password_confirmation
+	
+	before_save :encrypt_password
+	
+	def self.authenticate name, password
+    user = User.find_by_name name
+    if user && user.password == BCrypt::Engine.hash_secret(password, user.salt)
+      user
+    else
+      false
+    end
+  end
+  
+  protected
+	
+	def encrypt_password
+    if password_confirmation.present? and password.present?
+      self.salt = BCrypt::Engine.generate_salt
+      self.password = BCrypt::Engine.hash_secret password, salt
+    end       
+  end 
 end
