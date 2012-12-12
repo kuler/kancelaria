e8a7ce13ccdddf574307bcd717e7d5ccac90bd5e
kuler <kuler@cersoft.pl>
2012-12-12 02:40:28 +0100

    changing default root to calendar#index

--- config/routes.rb
+++ config/routes.rb
@@ -73,7 +73,7 @@ Kancelaria::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  root :to => 'home#index'
+  root :to => 'calendar#index'
 
   # See how all your routes lay out with "rake routes"
 
